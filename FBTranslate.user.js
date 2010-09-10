a13c78683d4c4aebb02033a0c52fda6331dd771e
no.good.at.coding <nogoodatcoding+github@gmail.com>
2010-09-10 17:58:43 -0400

    Added an empty userscript for Facebook Translation

---
+++ FBTranslate.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           FacebookTranslate
+// @namespace      com.nogoodatcoding.greasemonkey
+// @description    Translates text within Facebook posts into the user's language of choice
+// @include        http://*.facebook.com/*
+// @include        https://*.facebook.com/*
+// @version 0.1
+// ==/UserScript==
